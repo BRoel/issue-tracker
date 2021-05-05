2a1c3bc5fbbce8863a3d21be79bdec53f9cb9f2e
Brennan Roel <brennanroel@gmail.com>
2021-05-05 15:50:25 -0500

    edited profile layout

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,8 +8,8 @@ const Profile = () => {
     return (
         isAuthenticated && (
             <div>
+                <img src={user.picture} alt={user.name}/>
                 <h2>{user.name}</h2>
-                <p>{user.email}</p>
                 <JSONPretty data={user} />
             </div>
         )

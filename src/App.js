39ed84f7c8f6ca0a0fc3ef1d1775ef4a18b714ab
Brennan Roel <brennanroel@gmail.com>
2021-05-06 21:45:06 -0500

    added bootstrap react Navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavbar from './components/MyNavbar';
+
 
 function App() {
   const { isLoading } = useAuth0();
@@ -11,9 +14,10 @@ function App() {
 
   return (
     <>
-     <LoginButton />
-     <LogoutButton />
-     <Profile />
+      <MyNavbar />
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
     </>
   );
 }

49568b6b12aa036fc5dc59b9341296b51d61238e
Brennan Roel <brennanroel@gmail.com>
2021-05-14 13:28:40 -0500

    begining code for IssueForm

---
+++ src/components/IssueForm.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const IssueForm = props => {
+    const [title, setTitle] = useState('');
+    return (
+        <form>
+
+        </form>
+    );
+}
+
+export default IssueForm

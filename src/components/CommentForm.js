c07036c99da4d554df596448cfb2f181a8e2afd2
Brennan Roel <brennanroel@gmail.com>
2021-05-14 13:30:49 -0500

    added starter code for CommentForm

---
+++ src/components/CommentForm.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const CommentForm = props => {
+    const [content, setContent] = useState('');
+    return (
+        <form>
+
+        </form>
+    );
+}
+
+export default CommentForm
